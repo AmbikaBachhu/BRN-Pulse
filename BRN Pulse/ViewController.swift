e37e472d818a9e3804c818b51af9137e2060fb2d
Ambika <bachhuambika@gmail.com>
2019-08-31 12:59:04 +0530

    initial commit

---
+++ BRN Pulse/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  BRN Pulse
+//
+//  Created by B.Ambika on 8/31/19.
+//  Copyright © 2019 B.Ambika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //mngnbvm,h
+        //hfgnmnbvm
+    }
+
+
+}
+
